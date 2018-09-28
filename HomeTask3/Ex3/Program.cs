03f0075eead210769d51931f2794e6c27bd4b70e
DonVolkRam <success.vka@gmail.com>
2018-09-28 17:18:56 +0300

    Добавлен ввод дробей вида a/b

--- HomeTask3/Ex3/Program.cs
+++ HomeTask3/Ex3/Program.cs
@@ -30,26 +30,85 @@ namespace Ex3
 
         public static bool ZnamenatNotNull(int znamenatel)
         {
-            return true;
+            if (znamenatel == 0)
+                return false;
+            else
+                return true;
         }
+
         static void Main(string[] args)
         {
-            int a, b;
-            string s;
+            int a = 1, 
+                b = 1;
+
+            bool isIntChislit = true, 
+                isIntZnamenat = true, 
+                isNotNull = true,
+                isSlash = true,
+                isManySlashes = false;
+
+            string[] fractions;
+
             do
             {
-                Console.WriteLine("Введите числитель");
-                s = Console.ReadLine();
-            }
-            while (int.TryParse(s, out a));
-            do
-            {
-                Console.WriteLine("Введите знаменатель");
-                s = Console.ReadLine();
-            }
-            while (int.TryParse(s, out a)) ;
+                if (!isIntChislit || !isIntZnamenat || !isNotNull || !isSlash || isManySlashes)
+                {
+                    if (!isIntChislit)
+                        Console.WriteLine("Введите целый числитель");
+                    if (!isIntZnamenat)
+                        Console.WriteLine("Введите целый знаменатель");
+                    if (!isNotNull)
+                        Console.WriteLine("Знаменатель должен быть больше нуля");
+                    if (!isSlash)
+                        Console.WriteLine("Вы забыли про дробную черту");
+                    if (isManySlashes)
+                        Console.WriteLine("Вы ввели более одной дробной черты");
 
+                }
+                else
+                    Console.WriteLine("Введите дробь вида a/b , где а и b целые числа и b не равно 0");
+                
+                fractions = Console.ReadLine().Split('/');
+                isSlash = fractions.Length > 1 ? true : false;
+                isManySlashes = fractions.Length > 2 ? true : false;
 
+                if (!isSlash || isManySlashes)
+                    continue;
+
+                isIntChislit = int.TryParse(fractions[0], out a);
+                isIntZnamenat = int.TryParse(fractions[1], out b);
+                isNotNull = ZnamenatNotNull(b);                              
+            } 
+            while (!isIntChislit  || !isIntZnamenat || !isNotNull || !isSlash || isManySlashes);
+
+            Fraction drob1 = new Fraction(a, b);
+
+            Console.Write("Дробь выглядит так : ");
+            Console.WriteLine(drob1.ToString());
+
+/*
+                        do
+                        {
+                            Console.WriteLine("Введите числитель");
+                            s = Console.ReadLine();
+                            isInt = int.TryParse(s, out a);
+                        }
+
+                        while (!isInt);
+                        do
+                        {
+                            Console.WriteLine("Введите знаменатель");
+                            s = Console.ReadLine();
+                            isInt = int.TryParse(s, out b);
+                            notNull = ZnamenatNotNull(b);
+                        }
+                        while (!isInt || !notNull);
+            
+                        Fraction drob1 = new Fraction(a, b);
+
+                        Console.Write("Дробь выглядит так : ");
+                        Console.WriteLine(drob1.ToString());
+*/
             Console.ReadLine();
         }
     }
