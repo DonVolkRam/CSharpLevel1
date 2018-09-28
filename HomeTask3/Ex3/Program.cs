10bc87a10b67a227d397660fa56b09ba11308879
DonVolkRam <success.vka@gmail.com>
2018-09-28 15:51:37 +0300

    создание 3 задания

---
+++ HomeTask3/Ex3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+    class Fraction
+    {
+        int chislit;
+        int znamenat;
+
+        Fraction()
+        {
+            this.chislit = 1;
+            this.znamenat = 1;
+        }
+
+        Fraction(int a, int b)
+        {
+            this.chislit = a;
+            this.znamenat = b;
+        }
+       
+        public override string ToString()
+        {
+            return chislit +"/" + znamenat;
+        }
+
+        public static bool ZnamenatNotNull(int znamenatel)
+        {
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            string s;
+            do
+            {
+                Console.WriteLine("Введите числитель");
+                s = Console.ReadLine();
+            }
+            while (int.TryParse(s, out a));
+            do
+            {
+                Console.WriteLine("Введите знаменатель");
+                s = Console.ReadLine();
+            }
+            while (int.TryParse(s, out a)) ;
+
+
+            Console.ReadLine();
+        }
+    }
+}
